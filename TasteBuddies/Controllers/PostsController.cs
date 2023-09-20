f391fee392d6fddf80e01829cf25a64b3187ca6e
iworsham <15iworsham@gmail.com>
2023-09-20 15:18:11 -0400

    c

--- TasteBuddies/Controllers/PostsController.cs
+++ TasteBuddies/Controllers/PostsController.cs
@@ -1,12 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TasteBuddies.DataAccess;
+using TasteBuddies.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography;
 
 namespace TasteBuddies.Controllers
 {
     public class PostsController : Controller
     {
+        private readonly TasteBuddiesContext _context;
+
+        public PostsController(TasteBuddiesContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
+        {
+            var posts = _context.Posts.ToList();
+            return View(posts);
+        }
+        public IActionResult Feed()
         {
             return View();
         }
+        public IActionResult Create(Post post)
+        {
+
+            _context.Posts.Add(post);
+            _context.SaveChanges();
+
+
+
+            return RedirectToAction("Feed", new { id = post.Id });
+
+        }
+
     }
 }
